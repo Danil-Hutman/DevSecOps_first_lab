c57c02459169ae3e15c008b37ec3f11cb79b1804
Danil Hutman <daniahutman@gmail.com>
2026-03-12 21:41:30 +0200

    style(lint): fix flake8 E261 spacing before inline comment

--- src/app.py
+++ src/app.py
@@ -35,4 +35,4 @@ def cactus_list():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) # nosec B104
+    app.run(host="0.0.0.0", port=port)  # nosec B104
